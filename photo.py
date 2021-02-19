b8f83f369ead6c241dd102c2248325d7355ee92c
Zerrari <1660822219@qq.com>
2021-02-19 14:07:20 +0800

    second

--- photo.py
+++ photo.py
@@ -3,9 +3,6 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-target_url = 'http://www.netbian.com/'
-target_header = {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 FS'}
-
 def ana_web(url,header):
     response = requests.get(url,headers=header)
     response.encoding = 'gbk'
@@ -16,25 +13,42 @@ def ana_web(url,header):
 
 def setDir():
     print("你正处于的路径：")
-    print(os.path.abspath('.'))
-    now_path = os.path.abspath('.')
     print('\n')
-    dir_name = input('请输入你想保存的文件夹名称')
-    os.mkdir(dir_name)
-    os.chdir(now_path+'/'+dir_name)
+    os.chdir('/Users/zerrari/Pictures/wallpaper')
+    print(os.path.abspath('.'))
 
-def download_img():
-    tags=ana_web(target_url,target_header)
-    setDir()
-    nums = 0
+def check_img(input,check):
+    result = check in input
+    return result
+    
+def download_img(tags,nums,flag):
     for tag in tags:
-        nums = nums + 1
-        img = requests.get(tag['src']).content
-        with open(str(nums)+'.jpg','wb') as f:
-            f.write(img)
-            f.close()
+        print(tag.get('data-src'))
+        if tag.get('data-src') is None:
+            continue
+        else:
+            img = requests.get(tag.get('data-src')).content
+            with open('index' + str(flag) + '_' + str(nums)+'.jpg','wb') as f:
+                f.write(img)
+                f.close()
+                nums = nums + 1
 
-download_img()
+            
+target_url_p1 = 'https://wallhaven.cc/hot?page='
+target_header = {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 FS'}
+
+flag = 1
+index = int(input('请输入：\n'))
+nums = 1
+setDir()
+
+while flag <= index:
+    target_url = target_url_p1 + str(flag)
+    tags = ana_web(target_url,target_header)
+    download_img(tags,nums,flag)
+    nums = 1
+    flag = flag + 1
+
 
     
 
